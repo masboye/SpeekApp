6436e096ba9a9e5997dc42da811309241ec1d769
boy.setiawan <boye.setiawan@gmail.com>
2019-07-08 10:25:40 +0700

    Initial Commit

---
+++ SpeekApp/FirstViewController.swift
@@ -0,0 +1,20 @@
+//
+//  FirstViewController.swift
+//  SpeekApp
+//
+//  Created by boy setiawan on 08/07/19.
+//  Copyright © 2019 boy setiawan. All rights reserved.
+//
+
+import UIKit
+
+class FirstViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
---
+++ SpeekApp/SecondViewController.swift
@@ -0,0 +1,20 @@
+//
+//  SecondViewController.swift
+//  SpeekApp
+//
+//  Created by boy setiawan on 08/07/19.
+//  Copyright © 2019 boy setiawan. All rights reserved.
+//
+
+import UIKit
+
+class SecondViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
